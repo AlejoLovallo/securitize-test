3975032a9a6dabec6eee0492eb3fa93a81f72f6d
Alejo Lovallo <alejoe.lovallo@gmail.com>
2025-05-14 10:54:17 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Securitize test
+
+* Securitize interview assesment
+
+*******
+
+### Requirements
+
+General:
+
+* Node v23
+* Pnpm <>
+
+* Backend:
+
+* Contracts:
+
+* UI:
+
+
+### Code structure
+
+* Monorepo built with pnpm and turbo. 
+
+### Commands
+
+* Set up node version
+
+```
+```
+
+*******
+
+### Contracts
+
+### Backend
+
+* Please refer to further documentation at [backend](./backend/README.md)
+
+### UI
+
+* Please refer to further documentation at [frontend](./ui/README.md)
+
+********
+
+### Deployment
+
+* Dapp has been deployed at: 
---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import typescriptPlugin from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default [
+  {
+    ignores: ['.eslintrc.js'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+      },
+      globals: {
+        node: true,
+        jest: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...typescriptPlugin.configs.recommended.rules,
+      ...prettierPlugin.configs.recommended.rules,
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-duplicate-enum-values': 'off',
+    },
+  },
+]
